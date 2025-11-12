ec2749829d039a0e2988a68aa6a5845b48d3b780
brintonlee <99971541+brintonlee@users.noreply.github.com>
2025-11-12 15:41:39 +0800

    init template

---
+++ test_cpp/two-pointer.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+    //start here
+    int N;cin >> N;
+    vector<int> v(N);
+    for(auto &i:v) cin >> i;
+
+    cout << "Hello world!!!" << endl;
+}
