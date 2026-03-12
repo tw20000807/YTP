78dd85a00660b6915e252fc37822c7ca172c0c7f
tw20000807 <tw20000807@gmail.com>
2026-03-12 12:14:04 +0800

    fix matrix and bar

--- test_cpp/total.cpp
+++ test_cpp/total.cpp
@@ -4,11 +4,11 @@ using namespace std;
 int main(){
     int n = 10;
     vector< int > arr(n);
-    vector< vector< int > > a(n);
+    vector< vector< int > > a(n, vector< int >(n));
     for(int i = 0; i < n; ++i){
         arr[i] = 2 * i - n;
         for(int j = 0; j < n; ++j){
-            a[i].push_back(i * n + j);
+            a[i][j] = i * n + j;
         }
         
     }
